41b894bc77e9558cd02e4811b6f3796b96cafd54
Michael Hernando <mhernando334@gmail.com>
2023-12-05 11:08:01 -0500

    firebase initialization

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -1,6 +1,6 @@
 /* LandingPage.tsx */
 import { Button, Container, Typography, Box } from '@mui/material';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Link } from "react-router-dom";
 import {
     pageStyles,
